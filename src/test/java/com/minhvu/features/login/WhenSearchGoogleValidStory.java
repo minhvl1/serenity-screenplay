a3ae8da111c1a9fee210c4cad4bb1d37624d16f1
minhvl1 <minhvu890yahoo@gmail.com>
2023-06-23 11:18:31 +0700

    update script

---
+++ src/test/java/com/minhvu/features/login/WhenSearchGoogleValidStory.java
@@ -0,0 +1,58 @@
+package com.minhvu.features.login;
+
+import com.minhvu.tasks.OpenTheApplication;
+import com.minhvu.tasks.Search;
+import io.appium.java_client.android.nativekey.PressesKey;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.EnterValue;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
+import net.serenitybdd.screenplay.targets.Target;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.WithTag;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+
+@RunWith(SerenityRunner.class)
+public class WhenSearchGoogleValidStory {
+
+    public static final Target BUTTON_SEARCH =
+            Target.the("search button")
+                    .locatedBy("//textarea[@class='gLFyf']");
+
+    Actor minh= Actor.named("MinhVu");
+
+    @Managed(uniqueSession = true)
+    public WebDriver hisBrowser;
+
+
+
+    @Before
+    public void minhCanBrowseTheWeb() {
+        minh.can(BrowseTheWeb.with(hisBrowser));
+    }
+
+    @WithTag("SearchGoogle")
+    @Test
+    public void search_results_keyword_in_the_google() {
+        minh.wasAbleTo(
+                Open.url("https://www.google.com.vn/"),
+                Click.on(BUTTON_SEARCH),
+                Enter.theValue("youtube").into(BUTTON_SEARCH).thenHit(Keys.ENTER),
+                Ensure.thatTheCurrentPage().currentUrl().contains("youtube")
+
+        );
+
+
+    }
+}
